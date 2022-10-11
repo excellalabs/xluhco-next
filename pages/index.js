8c912f6a757cf40a97a9ab4673bf2a6bb64ddb4e
Shaun Decker <shaun.decker@excella.com>
2022-10-11 16:11:12 -0400

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { env } from 'process'
 import styles from '../styles/Home.module.css'
-import {getLinks} from '../lib/db'
 
 export default function Home() {
   return (
