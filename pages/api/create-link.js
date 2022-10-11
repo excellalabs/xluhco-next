56c07c057bf894b22225f4425cf060c33a2b2bb6
Shaun Decker <shaun.decker@excella.com>
2022-10-11 15:31:00 -0400

    Create and Delete api endpoints working

--- pages/api/create-link.js
+++ pages/api/create-link.js
@@ -1,24 +1,10 @@
 import {createLink} from '../../lib/db'
 
 export default async function handler(req, res) {
-    
-    // const { db } = await connectToDatabase();
-    // // Update link Data
-    // const { shortKey, linkUrl, userId, id } = req.body;
-    // await db.collection("links").updateOne(
-    //   { _id: ObjectId(id) },
-    //   {
-    //     $set: {
-    //       linkUrl,
-    //       shortKey,
-    //       userId
-    //     },
-    //   }
-    // );
-    // // Send a response
-    createLink();
+    const { linkUrl, userId} = req.body;
+    await createLink(linkUrl, userId);
     res.status(200).json({
-      data: await db.collection("links").findOne({ _id: ObjectId(id) }),
+      data: {},
       message: "Link updated successfully",
     });
-  }
+}
