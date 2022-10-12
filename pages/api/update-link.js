5508a78de1aff4e90ca1964b17fbc0d53cfd1203
Ian Robinson <ian@ianrobinson.net>
2022-10-12 10:05:15 -0700

    Linting indentation

--- pages/api/update-link.js
+++ pages/api/update-link.js
@@ -1,10 +1,10 @@
 import { updateLink } from '../../lib/db'
 
 export default async function handler(req, res) {
-    const { shortId, linkUrl } = req.body;
-    const updatedLink = await updateLink(shortId, linkUrl);
-    res.status(200).json({
-      data: { updatedLink },
-      message: "Link updated successfully",
-    });
+  const { shortId, linkUrl } = req.body;
+  const updatedLink = await updateLink(shortId, linkUrl);
+  res.status(200).json({
+    data: { updatedLink },
+    message: "Link updated successfully",
+  });
 }
