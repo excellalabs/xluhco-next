8ca5e93e8c40d001727414bc3aec1485111884b0
Ian Robinson <ian@ianrobinson.net>
2022-10-13 15:27:28 -0700

    Add tool to scrape old xhula links

---
+++ tools/scrape.js
@@ -0,0 +1,14 @@
+const scrapeFromOriginalXhula = () => {
+  let theTable = document.getElementsByClassName("list")[0].children;
+  let formattedData = [];
+  for (let i = 0; i < theTable.length; i += 1) {
+    let row = theTable[i];
+    formattedData.push({
+      'shortUrl': row.children[0].innerText.trim(),
+      'destinationUrl': row.children[1].innerText,
+    })
+  }
+  return formattedData;
+}
+
+scrapeFromOriginalXhula();
