fc8fa38924ea304a2c6dd1a61080a71b9c3d312c
Shaun Decker <shaun.decker@excella.com>
2022-10-13 14:19:29 -0400

    Adds ability to set custom slugs and replaces shortid with nanoid

--- pages/api/create-link.js
+++ pages/api/create-link.js
@@ -1,8 +1,8 @@
 import { createLink } from '../../lib/db'
 
 export default async function handler(req, res) {
-  const { linkUrl, userId } = req.body;
-  const createdLink = await createLink(linkUrl, userId);
+  const { linkUrl, shortId } = req.body;
+  const createdLink = await createLink(linkUrl, shortId);
   res.status(200).json({
     data: { createdLink },
     message: "Link created successfully",
