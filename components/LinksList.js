e66d8c482a7720bfecaa86784d9ef533aa071de9
Ian Robinson <ian@ianrobinson.net>
2022-10-14 10:47:09 -0700

    Improve mobile responsiveness

--- components/LinksList.js
+++ components/LinksList.js
@@ -6,15 +6,15 @@ export default function LinksList({links}){
         <div className="card my-3" key={i}>
           <div className="card-body">
             <div className="row g-4">
-              <div className="col-5">
+              <div className="col-lg-5">
                 <p>{link.url}</p>
               </div>
-              <div className="col-5">
+              <div className="col-lg-5">
                 <a class="fw-lighter float-end" href={`${process.env.HOST}/${link.shortId}`}>
-                  {`${process.env.HOST}/${link.shortId}`}
+                  {`http://www.xluh.co/${link.shortId}`}
                 </a>
               </div>
-                <a class="btn btn-primary col-2">Copy</a>
+                <a class="btn btn-primary col-lg-2">Copy</a>
             </div>
           </div>
           <div className="card-footer">
--- components/UrlForm.js
+++ components/UrlForm.js
@@ -3,10 +3,10 @@ export default function UrlForm({links}){
   return (
     <div className="mb-3 py-3">
       <div className="row">
-        <div className="col-lg-5 py-3">
+        <div className="col-lg-5 col-md-5 py-3">
           <input type="url" className="form-control" id="exampleFormControlInput1" placeholder="Target URL" />
         </div>
-        <div className="col-lg-5 py-3">
+        <div className="col-lg-5 col-md py-3">
           <div class="row g-3 align-items-center">
             <div class="col-auto">
               <label for="inputPassword6" class="col-form-label">http://www.xluh.co/</label>
@@ -17,7 +17,7 @@ export default function UrlForm({links}){
           </div>
         </div>
         <div class="col-lg-2 d-grid gap-2 py-3">
-          <button type="button" class="btn btn-primary mb-3">Copy</button>
+          <button type="button" class="btn btn-primary mb-3">Add</button>
         </div>
       </div>
     </div>
