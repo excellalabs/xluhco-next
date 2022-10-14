f7acb78074649aadf2b8fc2a77a44d3c4ddbdb1d
Ian Robinson <ian@ianrobinson.net>
2022-10-14 11:45:48 -0700

    Wrap fields in a form element

--- components/UrlForm.js
+++ components/UrlForm.js
@@ -1,25 +1,27 @@
 export default function UrlForm({links}){
 
   return (
-    <div className="mb-3 py-3">
-      <div className="row">
-        <div className="col-lg-5 col-md-5 py-3">
-          <input type="url" className="form-control" id="exampleFormControlInput1" placeholder="Target URL" />
-        </div>
-        <div className="col-lg-5 col-md py-3">
-          <div class="row g-3 align-items-center">
-            <div class="col-auto">
-              <label for="inputPassword6" class="col-form-label">http://www.xluh.co/</label>
-            </div>
-            <div class="col-auto">
-              <input type="userDesiredShortUrl" id="userDesiredShortUrl" class="form-control" aria-describedby="userDesiredShortUrl" />
+    <div className="pt-3">
+      <form action="/send-data-here" method="post">
+        <div className="row">
+          <div className="col-lg-5 col-md-5 py-3">
+            <input type="url" className="form-control" id="exampleFormControlInput1" placeholder="Target URL" />
+          </div>
+          <div className="col-lg-5 col-md py-3">
+            <div class="row g-3 align-items-center">
+              <div class="col-auto">
+                <label for="inputPassword6" class="col-form-label">http://www.xluh.co/</label>
+              </div>
+              <div class="col-auto">
+                <input type="userDesiredShortUrl" id="userDesiredShortUrl" class="form-control" aria-describedby="userDesiredShortUrl" />
+              </div>
             </div>
           </div>
+          <div class="col-lg-2 d-grid gap-2 py-3">
+            <button type="submit" class="btn btn-primary mb-3">Add</button>
+          </div>
         </div>
-        <div class="col-lg-2 d-grid gap-2 py-3">
-          <button type="button" class="btn btn-primary mb-3">Add</button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
