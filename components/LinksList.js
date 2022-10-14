9177a5b5a0718b754b7e2c36ecd56a8b1fe87d58
Ian Robinson <ian@ianrobinson.net>
2022-10-13 17:00:24 -0700

    Rough in how the card will look

--- components/LinksList.js
+++ components/LinksList.js
@@ -5,10 +5,23 @@ export default function LinksList({links}){
       {links.map((link, i) => (
         <div className="card" key={i}>
           <div className="card-body">
-            <p>{link.url}</p>
-            <a class="btn btn-primary" href={`${process.env.HOST}/${link.shortId}`}>
-              {`${process.env.HOST}/${link.shortId}`}
-            </a>
+            <div className="row g-4">
+              <div className="col-5">
+                <p>{link.url}</p>
+              </div>
+              <div className="col-5">
+                <a class="fw-lighter float-right" href={`${process.env.HOST}/${link.shortId}`}>
+                  {`${process.env.HOST}/${link.shortId}`}
+                </a>
+              </div>
+                <a class="btn btn-primary col-2">Copy</a>
+            </div>
+          </div>
+          <div className="card-footer">
+            <p>
+              Want to track your links?
+              <a className="btn btn-success float-right">Get Started</a>
+            </p>
           </div>
         </div>
       ))}
