76c31664790cad7280b75f7b4eb428ca61b8439a
Ian Robinson <ian@ianrobinson.net>
2022-10-19 13:48:34 -0700

    Change buttons to excella colors and add an about page

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+export default function About(){
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center py-5">
+          <h1>About Xhula.co</h1>
+          <p>Xluh is a tool for Excellians to make sharing URLs easier.</p>
+          <p>Have fun!</p>
+          <a href="/" className="btn btn-primary">Explore</a>
+        </div>
+      </div>
+
+    </div>
+  )
+}
