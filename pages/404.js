df1c831058b7046f2c25b8c983b72603aed0e08d
Shaun Decker <shaun.decker@excella.com>
2022-10-21 11:59:16 -0400

    404 Redirect if slug doesnt exist

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+
+export default function NotFound(){
+    return (
+      <div className="container">
+        <Header />
+        <div className="row">
+          <div className="col text-center py-5">
+            <h1>Slug Not Found</h1>
+          </div>
+        </div>
+  
+      </div>
+    )
+  }
