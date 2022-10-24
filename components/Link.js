6b89ce61540f5e063463fdc3523c73b2e13a4453
Ian Robinson <ian@ianrobinson.net>
2022-10-24 13:23:23 -0400

    Further simplify and optimize for all screen sizes

--- components/Link.js
+++ components/Link.js
@@ -20,22 +20,22 @@ export default function Link({link, onDelete = f => f}){
   }
 
   return(
-    <div className="row p-2">
-      <div className="col py-2">
-        <h5 className="m-0">{link.url}</h5>
+    <div className="row">
+      <div className="col-12 d-flex justify-content-center py-2 justify-content-sm-start col-sm-8 text-break">
+        <h5>{link.url}</h5>
       </div>
-      <div className="col d-flex justify-content-end py-2">
-        <a target="_blank" className="text-secondary" href={`/${link.slug}`}>
+      <div className="col-12 d-flex justify-content-center py-2 col-sm-4 justify-content-sm-end">
+        <a target="_blank" className="text-secondary mx-3" href={`/${link.slug}`}>
             {`${process.env.HOST}/${link.slug}`}
         </a>
-      </div>
-      <div className="col d-flex justify-content-end py-2">
-        <a className="btn btn-success "
-            onClick={() => copyLinkToClipboard(link.slug)}>
-          Copy
+        <a onClick={() => copyLinkToClipboard(link.slug)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+          </svg>
         </a>
         <Dropdown>
-          <Dropdown.Toggle variant="link" className="text-secondary" bsPrefix="p-0" id={`dropdown_${link.slug}`}>
+          <Dropdown.Toggle variant="link" className="text-secondary mr-3" bsPrefix="p-0" id={`dropdown_${link.slug}`}>
             <BsThreeDotsVertical />
           </Dropdown.Toggle>
 
